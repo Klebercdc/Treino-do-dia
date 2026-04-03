af62d2e6ed680850e94de677f8f3773525a899bb
Klebercdc <100015736+Klebercdc@users.noreply.github.com>
2026-04-02 23:25:34 -0300

    hardening(elite): stabilize vercel deploy and consolidate serverless surface to hobby-safe

--- api/system.js
+++ api/system.js
@@ -7,6 +7,9 @@ var adminDiagnosticsHandler = require('../src/server/legacy/admin-diagnostics');
 var machineHandler = require('../src/server/legacy/machine');
 var cronScraperHandler = require('../src/server/legacy/cron-scraper');
 var pastorDiagnosticoHandler = require('../src/server/legacy/pastor-diagnostico');
+var adminImportExercisesHandler = require('../src/server/internal/http/admin-import-exercises');
+var adminImportExercisesStatusHandler = require('../src/server/internal/http/admin-import-exercises-status');
+var adminImportExercisesAutoHandler = require('../src/server/internal/http/admin-import-exercises-auto');
 var https = require('https');
 
 var SUPABASE_URL = (process.env.SUPABASE_URL || '').replace(/\/$/, '');
@@ -172,6 +175,13 @@ module.exports = function(req, res) {
       return cronScraperHandler(req, res);
     case 'pastor-diagnostico':
       return pastorDiagnosticoHandler(req, res);
+
+    case 'admin-import-exercises':
+      return adminImportExercisesHandler(req, res);
+    case 'admin-import-exercises-status':
+      return adminImportExercisesStatusHandler(req, res);
+    case 'admin-import-exercises-auto':
+      return adminImportExercisesAutoHandler(req, res);
     default:
       return res.status(404).json({ error: 'rota não encontrada' });
   }
